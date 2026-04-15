0249677ebf6c19a01513a8aa6d6d7b93f6014ffe
celestialhaven <jonzmabz04@gmail.com>
2026-04-15 19:52:38 +0800

    update

--- next.config.mjs
+++ next.config.mjs
@@ -1,11 +1,10 @@
-/** @type {import('next').NextConfig} */
 const nextConfig = {
-  typescript: {
-    ignoreBuildErrors: true,
-  },
+  output: "export",
   images: {
     unoptimized: true,
   },
+  basePath: "/ercpa-draft",
+  assetPrefix: "/ercpa-draft/",
 }
 
 export default nextConfig
